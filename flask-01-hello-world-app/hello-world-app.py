864e745607e04bc44eb5808be0591788a1e1db75
guidot <6181359+guidot@users.noreply.github.com>
2024-10-18 12:02:39 +0200

    First Flask and Jinja samples

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,18 @@
-from flask import Flask 
+from flask import Flask
+
 app = Flask(__name__)
 
 
+@app.route("/")
+def root():
+    return "<html>Hi! <a href=/2>Goto 2</a></html>"
 
 
+@app.route("/2")
+def second():
+    return "<html>See me? <a href=/>Go back</a></html>"
 
 
-if __name__ == '__main__':
-
-     app.run(debug=True, port=30000)
-     # app.run(host= '0.0.0.0', port=8081)  # for ec2 usage
+if __name__ == "__main__":
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8081)  # for ec2 usage
--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -3,12 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/")
+def head():
+    return render_template("index.html", n1=2, n2="BLA")
 
 
+@app.route("/calc")
+def number():
+    num1 = 11
+    num2 = 12
+    return render_template("body.html", value1=num1, value2=num2, sum=num1 + num2)
 
 
-
-
-if __name__== "__main__":
-     app.run(debug=True, port=30000)
-     # app.run(host= '0.0.0.0', port=8081)  # for ec2 instances
+if __name__ == "__main__":
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8081)  # for ec2 instances
